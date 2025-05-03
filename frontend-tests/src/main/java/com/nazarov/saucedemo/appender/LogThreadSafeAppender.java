15deb456c0bb4a0c41e7b4c1f420019876525846
Andrii Nazarov <andreu.nazarov@gmail.com>
2025-05-03 15:53:14 +0200

    Updated threadsafe logger to format log messages

--- frontend-tests/src/main/java/com/nazarov/saucedemo/appender/LogThreadSafeAppender.java
+++ frontend-tests/src/main/java/com/nazarov/saucedemo/appender/LogThreadSafeAppender.java
@@ -1,5 +1,6 @@
 package com.nazarov.saucedemo.appender;
 
+import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import java.util.ArrayList;
@@ -7,19 +8,28 @@ import java.util.List;
 
 public class LogThreadSafeAppender extends AppenderBase<ILoggingEvent> {
 
-  static ThreadLocal<List<ILoggingEvent>> threadLocal = new ThreadLocal<>();
+  static ThreadLocal<List<String>> threadLocal = new ThreadLocal<>();
+  private final PatternLayout layout = new PatternLayout();
+
+  @Override
+  public void start() {
+    super.start();
+    layout.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
+    layout.setContext(getContext());
+    layout.start();
+  }
 
   @Override
   public void append(ILoggingEvent e) {
-    List<ILoggingEvent> events = threadLocal.get();
+    List<String> events = threadLocal.get();
     if (events == null) {
       events = new ArrayList<>();
       threadLocal.set(events);
     }
-    events.add(e);
+    events.add(layout.doLayout(e));
   }
 
-  public static List<ILoggingEvent> getEvents() {
+  public static List<String> getEvents() {
     return threadLocal.get();
   }
 
--- frontend-tests/src/main/java/com/nazarov/saucedemo/extensions/LogExt.java
+++ frontend-tests/src/main/java/com/nazarov/saucedemo/extensions/LogExt.java
@@ -2,15 +2,10 @@ package com.nazarov.saucedemo.extensions;
 
 import static java.util.Objects.nonNull;
 
-import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.nazarov.saucedemo.appender.LogThreadSafeAppender;
 import io.qameta.allure.Allure;
 import java.io.ByteArrayInputStream;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
@@ -26,23 +21,11 @@ public class LogExt implements BeforeEachCallback, AfterEachCallback {
 
   @Override
   public void afterEach(ExtensionContext extensionContext) {
-    List<ILoggingEvent> events = LogThreadSafeAppender.getEvents();
+    List<String> events = LogThreadSafeAppender.getEvents();
     byte[] log = "Log output is empty".getBytes();
     if (nonNull(events)) {
-      log = events.stream()
-          .map(eventFormatter)
-          .collect(Collectors.joining(System.lineSeparator(), "", System.lineSeparator()))
-          .getBytes();
+      log = String.join("", events).getBytes();
     }
     Allure.addAttachment("log", "text/plain", new ByteArrayInputStream(log), ".log");
   }
-
-  private static final Function<ILoggingEvent, String> eventFormatter = event -> {
-    var formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS").withZone(ZoneId.systemDefault());
-    return formatter.format(event.getInstant())
-        + " "
-        + event.getLoggerName()
-        + " "
-        + event.getFormattedMessage();
-  };
 }
